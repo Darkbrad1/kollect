9e28f3927cffc429b32460ff3dbe70d199b6fc73
darkbrad1 <bradleyandrews861@gmail.com>
2026-04-15 16:50:48 -0400

    updated the loading to be a skeleton loading

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,6 +6,7 @@ import CardView from "~/components/CardView"; // Component for card-style manga 
 import DetailedView from "~/components/DetailedView"; // Component for detailed manga display
 
 import { useAppState } from "./AppStateProvider"; // Custom hook to access shared app state
+import { Skeleton } from "./ui/skeleton";
 
 export default function RenderMangas() {
     // Get the current UI state from context
@@ -14,10 +15,17 @@ export default function RenderMangas() {
     // Fetch manga data from Convex using the listManga query
     // This will be undefined at first while the query is loading
     const mangas = useQuery(api.manga.listManga);
-
+    // const mangas = undefined;
     // Show a loading message until the query returns data
     if (mangas === undefined) {
-        return <div>Loading...</div>;
+        return (
+            <div className="w-full flex flex-wrap gap-3 mx-auto">
+                {/* Loading... */}
+                {Array.from({ length: 10 }).map((_, i) => (
+                    <Skeleton key={i} className="aspect-[1/1.5] h-[218px]" />
+                ))}
+            </div>
+        );
     }
 
     // Filter manga by title when the user types in the search box
@@ -34,29 +42,19 @@ export default function RenderMangas() {
     // - otherwise, show manga filtered by search text
     const inputMangas = !search ? filteredMangas : searchedMangas;
 
-    // Decide which component to render based on the selected view
-    // function renderView() {
-    //   switch (view) {
-    //     case "Card":
-    //       // Render manga in card format
-    //       return <CardView data={inputMangas} />
-
-    //     case "Detailed":
-    //       // Render manga in detailed list format
-    //       return <DetailedView data={inputMangas} />
-
-    //     default:
-    //       // Fallback in case view has an unexpected value
-    //       return null
-    //   }
-    // }
-
     // Render whichever view was selected
     return (
-        <div className="w-full h-full flex flex-wrap gap-3 mx-auto">
+        <div className="w-full flex flex-wrap gap-3 mx-auto">
             {inputMangas.map((data) => (
                 // Use the document ID as the React key for stable rendering
-                <Card key={data._id} url={data.site_url} id={data._id} image={data.cover_url} chapter={`${data.chapter_number}`} title={data.display_title}/>
+                <Card
+                    key={data._id}
+                    url={data.site_url}
+                    id={data._id}
+                    image={data.cover_url}
+                    chapter={`${data.chapter_number}`}
+                    title={data.display_title}
+                />
             ))}
         </div>
     );
